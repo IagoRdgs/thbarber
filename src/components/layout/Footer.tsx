02966a86de811990708a896521cbe1c3d8cdec37
IagoRdgs <iagoandre567@gmail.com>
2025-10-31 01:07:05 -0300

    :recycle: style: correção na borda/separador

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     const linkBaseStyle = "hover:text-yellow-mid hover:underline active:text-yellow-mid active:underline transition-all duration-300";
 
-    return <footer className="flex flex-col items-center mt-16 mb-8 space-y-4">
+    return <footer className="flex flex-col items-center mt-16 mb-8 space-y-4 border-t border-tertiary-p pt-4 mx-4">
         <div className="flex space-x-4 text-xs text-secondary-p">
             <a className={linkBaseStyle} href="#">Política de privacidade</a>
             <a className={linkBaseStyle} href="#">Termos de uso</a>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 export default function Header() {
     return (
         <header className="w-full flex flex-col items-center px-4">
-            <div className="flex w-full justify-center items-center border-b border-tertiary py-4 mb-16">
+            <div className="flex w-full justify-center items-center border-b border-tertiary-p py-4 mb-16">
                 <div className="w-full flex space-x-2">
                     <img src={scissors} alt="tesoura amarela" />
                     <span className="font-bold text-xl">TH Barbearia</span>
