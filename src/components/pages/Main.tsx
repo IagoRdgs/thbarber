3c383c0ce2910089291f6b45b97cb834905c889c
IagoRdgs <iagoandre567@gmail.com>
2025-11-16 01:06:30 -0300

    :recycle: refactor: export do serviceInfo para reutilização

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -7,41 +7,41 @@ import user from "../../assets/client1.png";
 import EvaluationCard from "../layout/EvaluationCard";
 import Footer from "../layout/Footer";
 
-type serviceInfo = {
+export type serviceInfo = {
     service: string;
     price: number;
     description: string;
     duration: number;
 };
 
-export default function Main() {
-    const services: serviceInfo[] = [
-        {
-            service: "Corte social",
-            price: 12,
-            description: "Corte simples com máquina e tesoura",
-            duration: 30
-        },
-        {
-            service: "Corte degradê",
-            price: 15,
-            description: "Corte com acabamento detalhado e personalizado",
-            duration: 45
-        },
-        {
-            service: "Barba",
-            price: 5,
-            description: "Fazer a barba e alinhamento/acabamento",
-            duration: 10
-        },
-        {
-            service: "Corte + Barba",
-            price: 20,
-            description: "Corte com acabamento detalhado + barba",
-            duration: 55
-        }
-    ];
+export const services: serviceInfo[] = [
+    {
+        service: "Corte social",
+        price: 12,
+        description: "Corte simples com máquina e tesoura",
+        duration: 30
+    },
+    {
+        service: "Corte degradê",
+        price: 15,
+        description: "Corte com acabamento detalhado e personalizado",
+        duration: 45
+    },
+    {
+        service: "Barba",
+        price: 5,
+        description: "Fazer a barba e alinhamento/acabamento",
+        duration: 10
+    },
+    {
+        service: "Corte + Barba",
+        price: 20,
+        description: "Corte com acabamento detalhado + barba",
+        duration: 55
+    }
+];
 
+export default function Main() {
     const evaluations: evaluationProps[] = [
         {
             photo: user,
@@ -71,7 +71,7 @@ export default function Main() {
             <Header />
             <section className="my-16">
                 <h2 className="text-primary-p text-xl px-8" id="services">Serviços</h2>
-                <div className="px-4 mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="px-4 mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                     {services.map((service => (
                         <ServiceCard
                             key={service.service}
