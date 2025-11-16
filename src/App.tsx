d94a1e73e2f697a48415bba4a163f8c4bea8a948
IagoRdgs <iagoandre567@gmail.com>
2025-11-16 01:03:24 -0300

    :package: package: instalação do react router dom e do dayjs para manipulação das datas de agendamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/pages/Main';
+import Scheduling from './components/pages/Scheduling';
+import Nav from './components/layout/Nav';
 
 function App() {
   return (
-    <>
-      <Main />
-    </>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route index Component={Main} />
+        <Route path='/scheduling' Component={Scheduling} />
+      </Routes>
+    </Router>
   )
 }
 
