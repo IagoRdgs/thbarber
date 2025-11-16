cbf8b479f2486e42515e8db741522e5b180c6c44
IagoRdgs <iagoandre567@gmail.com>
2025-11-16 01:06:07 -0300

    :recycle: refactor: add função de onclick como prop do service card

--- src/components/layout/ServiceCard.tsx
+++ src/components/layout/ServiceCard.tsx
@@ -5,27 +5,31 @@ type serviceProps = {
     price: number;
     description: string;
     duration: number;
+    onClick?: () => void;
+    selected?: boolean;
 };
 
 export default function ServiceCard({
     service,
     price,
     description,
-    duration
+    duration,
+    onClick,
+    selected
 }: serviceProps) {
     return (
-        <div className="bg-secondary-bg border border-yellow-mid rounded-lg px-4 py-8 space-y-4
-            hover:scale-103 hover:shadow-lg hover:shadow-yellow-mid/30
-            active:scale-103 active:shadow-lg active:shadow-yellow-mid/30
-            focus:scale-105 focus:shadow-lg focus:shadow-yellow-mid/30 outline-none
-            transition-all duration-300"
+        <div className={`bg-secondary-bg border border-yellow-mid rounded-lg px-4 py-8 space-y-4
+            hover:scale-102 hover:shadow-lg hover:shadow-yellow-mid/30
+            active:scale-102 active:shadow-lg active:shadow-yellow-mid/30
+            focus:scale-102 focus:shadow-lg focus:shadow-yellow-mid/30 outline-none
+            transition-all duration-300 ${selected ? 'border-2 scale-102 shadow-lg shadow-yellow-mid/30' : 'border'}`}
             tabIndex={0}
+            onClick={onClick}
         >
             <div className="flex justify-between items-center">
                 <span>{service}</span>
                 <span className="text-yellow-full font-medium text-lg">{price.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}</span>
             </div>
-
 
             <div className="text-tertiary-p text-sm space-y-2">
                 <p>{description}</p>
